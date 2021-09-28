63e277903d5d99ca1f83621ada4d2b7e72e136ff
Monique Rousseau <moniqueprousseau@icloud.com>
2021-09-28 16:31:25 -0500

    initial commit of users with bank accounts

---
+++ Fundamentals/oop/users_with_bank_accounts.py
@@ -0,0 +1,76 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = BankAccount(int_rate=0.03, balance=0)
+    
+    def deposit(self,amount):
+        self.account.balance += amount
+        return self
+    
+    def withdraw(self,amount):
+        if BankAccount.can_withdraw(self.account.balance,amount):
+            self.account.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.account.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print(self.account.balance)
+        return self
+
+    def yield_interest(self):
+        if self.account.balance > 0:
+            self.account.balance *= (1 + self.account.int_rate)
+        return self
+
+    @staticmethod
+    def can_withdraw(balance, amount):
+        if balance - amount < 0:
+            return False
+        else:
+            return True
+
+
+
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.int_rate = 0.03
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if BankAccount.can_withdraw(self.balance,amount):
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(self.balance)
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance *= (1 + self.int_rate)
+        return self
+
+    @staticmethod
+    def can_withdraw(balance, amount):
+        if balance - amount < 0:
+            return False
+        else:
+            return True
+
+patrick = BankAccount("Patrick", 0)
+patrick.deposit(100).deposit(500).deposit(175).withdraw(25).yield_interest().display_account_info()
+
+leslie = BankAccount("Leslie", 0)
+leslie.deposit(1000).deposit(500).withdraw(150).withdraw(25).withdraw(50).withdraw(250).yield_interest().display_account_info()
+
+monique = User("Monique")
+monique.deposit(100).display_account_info()
